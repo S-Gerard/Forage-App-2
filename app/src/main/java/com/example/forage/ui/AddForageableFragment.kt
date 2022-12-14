90d79c163dd71322a94e39bac3b5d4b66aa191c3
Shea Gerard <shea.gerard@outlook.com>
2022-12-14 12:30:26 -0700

    Completed data, model, ui, and base application files

--- app/src/main/java/com/example/forage/ui/AddForageableFragment.kt
+++ app/src/main/java/com/example/forage/ui/AddForageableFragment.kt
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 package com.example.forage.ui
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -24,10 +23,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.forage.BaseApplication
 import com.example.forage.R
 import com.example.forage.databinding.FragmentAddForageableBinding
 import com.example.forage.model.Forageable
 import com.example.forage.ui.viewmodel.ForageableViewModel
+import com.example.forage.ui.viewmodel.ForageableViewModelFactory
 
 
 /**
@@ -49,7 +50,11 @@ class AddForageableFragment : Fragment() {
     // TODO: Refactor the creation of the view model to take an instance of
     //  ForageableViewModelFactory. The factory should take an instance of the Database retrieved
     //  from BaseApplication
-    private val viewModel: ForageableViewModel by activityViewModels()
+    private val viewModel: ForageableViewModel by activityViewModels{
+        ForageableViewModelFactory(
+            (activity?.applicationContext as BaseApplication).database.forageableDao()
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -68,6 +73,10 @@ class AddForageableFragment : Fragment() {
 
             // TODO: Observe a Forageable that is retrieved by id, set the forageable variable,
             //  and call the bindForageable method
+            viewModel.forageable(id).observe(viewLifecycleOwner) {
+                forageable = it
+                bindForageable(it)
+            }
 
             binding.deleteBtn.visibility = View.VISIBLE
             binding.deleteBtn.setOnClickListener {
